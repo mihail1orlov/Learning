e4ebf69c4333cd40b6a7aa76704ed2e588b06b68
Mihail Orlov <mihail_orlov@hotmail.com>
2021-07-14 01:39:28 +0300

    TS interface

--- typeScript/index.ts
+++ typeScript/index.ts
@@ -1,3 +1,20 @@
+interface Person {
+    first: string,
+    last: string,
+    [key: string]: any
+}
+
+const person1: Person = {
+    first: 'Jeff',
+    last: 'Delaney'
+}
+
+const person2: Person = {
+    first: 'Usain',
+    last: 'Bolt',
+    fast: true
+}
+//-----
 type Style = 'bold' | 'italic' | 23;
 
 let font: Style;
