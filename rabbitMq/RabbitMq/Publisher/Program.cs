06b885dad3258fb310d08566761fd6406bff2539
Mihail Orlov <mihail_orlov@hotmail.com>
2021-11-21 13:03:30 +0200

    RabbitMq added publisher

--- rabbitMq/RabbitMq/Publisher/Program.cs
+++ rabbitMq/RabbitMq/Publisher/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory connectionFactory = new()
+{
+    Uri = new Uri("amqp://guest:guest@192.168.1.203:5672")
+};
+
+using (var connection = connectionFactory.CreateConnection())
+using(var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "dev-queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+    string message = "Message form publicher";
+    byte[] body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "", routingKey: "dev-queue", basicProperties: null, body: body);
+}
+
+Console.WriteLine($"Message was sent to Default Exchange");
