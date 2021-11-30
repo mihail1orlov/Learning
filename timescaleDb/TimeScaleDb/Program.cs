c4018ba24806f20c738729b6ffbb9710fe1f189f
Mihail Orlov <mihail_orlov@hotmail.com>
2021-11-30 16:38:45 +0200

    TimescaleDb Connecting .NET to TimescaleDB

--- timescaleDb/TimeScaleDb/Program.cs
+++ timescaleDb/TimeScaleDb/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Create a new instance of our helper class. This class
+// contains all of the methods for interacting with
+// TimescaleDB for this tutorial
+TimescaleHelper ts = new(host: "192.168.1.202", password: "12344321");
+
+// Procedure - Connecting .NET to TimescaleDB:
+// Verify that the program can connect
+// to the database and that TimescaleDB is installed!
+ts.CheckDatabaseConnection();
+
+Console.ReadKey();
---
+++ timescaleDb/TimeScaleDb/TimescaleHelper.cs
@@ -0,0 +1,73 @@
+﻿using Npgsql;
+// This class contains all of the methods needed to complete the
+// quick-start, providing a sample of each database operation in total
+// to refer to later.
+public class TimescaleHelper
+{
+    private static string Host = "";
+    private static string User = "";
+    private static string DBname = "";
+    private static string Password = "";
+    private static string Port = "";
+    private static string conn_str = "";
+
+    //
+    // This is the constructor for our TimescaleHelper class
+    //
+    public TimescaleHelper(string host = "localhost",
+        string user = "postgres",
+        string dbname = "postgres",
+        string password = "postgres",
+        string port = "5432")
+    {
+        Host = host;
+        User = user;
+        DBname = dbname;
+        Password = password;
+        Port = port;
+        // Build connection string using the parameters above
+        conn_str = $"Server={Host};Username={User};Database={DBname};Port={Port};Password={Password};SSLMode=Prefer";
+    }
+
+    //
+    // Procedure - Connecting .NET to TimescaleDB:
+    // Check the connection TimescaleDB and verify that the extension
+    // is installed in this database
+    //
+    public void CheckDatabaseConnection()
+    {
+        // get one connection for all SQL commands below
+        using (var conn = getConnection())
+        {
+
+            var sql = "SELECT default_version, comment FROM pg_available_extensions WHERE name = 'timescaledb';";
+
+            using (var cmd = new NpgsqlCommand(sql, conn))
+            {
+                using NpgsqlDataReader rdr = cmd.ExecuteReader();
+
+                if (!rdr.HasRows)
+                {
+                    Console.WriteLine("Missing TimescaleDB extension!");
+                    conn.Close();
+                    return;
+                }
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine("TimescaleDB Default Version: {0}\n{1}", rdr.GetString(0), rdr.GetString(1));
+                }
+                conn.Close();
+            }
+        }
+
+    }
+
+    // Helper method to get a connection for the execute function
+    private NpgsqlConnection getConnection()
+    {
+        var connection = new NpgsqlConnection(conn_str);
+        connection.Open();
+        return connection;
+    }
+}
