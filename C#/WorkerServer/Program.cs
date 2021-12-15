9bb645f5a76824579452f81351c7d8909ae937cb
Mihail Orlov <mihail_orlov@hotmail.com>
2021-12-15 05:11:09 +0200

    C# worker server has been added

---
+++ C#/WorkerServer/Program.cs
@@ -0,0 +1,10 @@
+using WorkerServer;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
