6986541de2b97e67de3dcb4b828c865aab21615d
Mihail Orlov <mihail_orlov@hotmail.com>
2021-12-15 12:25:38 +0200

    C# Dapper and Sql. Created data access layer

---
+++ C#/MinimalApiDemoApp/DataAccess/Data/UserData.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Models;
+
+namespace DataAccess.Data;
+
+public class UserData : IUserData
+{
+    private readonly ISqlDataAccess _db;
+
+    public UserData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<IEnumerable<UserModel>> GetUsers()
+        => _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new { });
+
+    public async Task<UserModel?> GetUser(int id)
+    {
+        var result = await _db.LoadData<UserModel, dynamic>("dbo.spUser_Get", new {Id = id});
+        return result.FirstOrDefault();
+    }
+
+    public async Task InsertUser(UserModel user) =>
+        await _db.SaveData("dbo.spUser_Insert", new {user.FirstName, user.LastName, user.Age});
+
+    public async Task UpdateUser(UserModel user) =>
+        await _db.SaveData("dbo.spUser_Update", user);
+
+    public async Task DeleteUser(int id) => await _db.SaveData("dbo.spUser_Delete", id);
+}
---
+++ C#/MinimalApiDemoApp/DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess.DbAccess;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration _configuration;
+
+    public SqlDataAccess(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<IEnumerable<T>> LoadData<T, TP>(string storedProcedure, TP parameters, string connectionId = "Default")
+    {
+        await using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionId));
+        return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Default")
+    {
+        await using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionId));
+        await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+}
