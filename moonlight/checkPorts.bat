03b4bcc5db1f90ec7cf3dde521973805ca7fa7ab
Mihail Orlov <mihail_orlov@hotmail.com>
2022-05-30 22:11:18 +0300

    Added moonlight check ports bash file

---
+++ moonlight/checkPorts.bat
@@ -0,0 +1,15 @@
+start telnet kipish.fun 47984
+timeout 3
+start telnet kipish.fun 47989
+timeout 3
+start telnet kipish.fun 48010
+timeout 3
+start telnet kipish.fun 5353
+timeout 3
+start telnet kipish.fun 47998
+timeout 3
+start telnet kipish.fun 47999
+timeout 3
+start telnet kipish.fun 48000
+timeout 3
+start telnet kipish.fun 48002
