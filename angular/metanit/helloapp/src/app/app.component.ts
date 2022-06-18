d6cea268463bc253379f0f4d8189bebafba56bb0
Mihail Orlov <mihail_orlov@hotmail.com>
2022-06-17 22:35:19 -0700

    Angular. Metanit. Ch1_2 Project, helloapp project

---
+++ angular/metanit/helloapp/src/app/app.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core';
+     
+@Component({ // функция декоратор, ассоциирует метаданные с классом компонента AppComponent
+    selector: 'my-app', // селектор css для HTML-элемента
+    
+    // шаблон, который указывает, как надо визуализировать компонент
+    template: `<label>Введите имя:</label>
+                 <input [(ngModel)]="name" placeholder="name">
+                 <h1>Добро пожаловать {{name}}!</h1>`
+})
+export class AppComponent {
+    name= '';
+}
