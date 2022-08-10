8dc8d023a205bb94859da4477611572fe9b9fbaa
Mihail Orlov <mihail_orlov@hotmail.com>
2022-08-10 09:53:41 +0300

    Added check sum application

---
+++ C#/CheckSumApp/CheckSum.cs
@@ -0,0 +1,12 @@
+﻿namespace CheckSumApp;
+
+internal class CheckSum
+{
+    public string Get(string path)
+    {
+        using var md5 = System.Security.Cryptography.MD5.Create();
+        using var stream = File.OpenRead(path);
+        var hash = md5.ComputeHash(stream);
+        return BitConverter.ToString(hash).Replace("-", "");
+    }
+}
---
+++ C#/CheckSumApp/Program.cs
@@ -0,0 +1,6 @@
+﻿using CheckSumApp;
+
+var checkSum = new CheckSum();
+var path = @"D:\soft\WinPE10_Sergei_Strelec_x86_x64_2016.03.25.iso";
+var cs = checkSum.Get(path);
+Console.WriteLine(cs);
