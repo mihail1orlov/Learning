80157713c2628087fedfb15baa741c0079cd193d
Mihail Orlov <mihail_orlov@hotmail.com>
2023-03-14 13:11:04 +0200

    Gpt. Added unitTest ICloneableMemberwiseAppTests prj

---
+++ C#/ICloneableApp/ICloneableMemberwiseApp/DeepCopyExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+
+public static class DeepCopyExtensions
+{
+    public static T DeepClone<T>(this T source)
+    {
+        if (source == null)
+        {
+            return default(T);
+        }
+
+        var type = source.GetType();
+
+        // Value types and strings can be copied directly
+        if (type.IsValueType || type == typeof(string))
+        {
+            return source;
+        }
+
+        // Arrays must be copied element by element
+        if (type.IsArray)
+        {
+            var elementType = type.GetElementType();
+            var sourceArray = source as Array;
+            var length = sourceArray.Length;
+            var targetArray = Array.CreateInstance(elementType, length);
+
+            for (int i = 0; i < length; i++)
+            {
+                var element = sourceArray.GetValue(i);
+                var clonedElement = DeepClone(element);
+                targetArray.SetValue(clonedElement, i);
+            }
+
+            return (T)Convert.ChangeType(targetArray, type);
+        }
+
+        // Objects with nested reference types must be copied recursively
+        var target = Activator.CreateInstance(type);
+
+        foreach (var field in type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+        {
+            var fieldValue = field.GetValue(source);
+            var clonedFieldValue = DeepClone(fieldValue);
+            field.SetValue(target, clonedFieldValue);
+        }
+
+        return (T)target;
+    }
+}
---
+++ C#/ICloneableApp/ICloneableMemberwiseAppTests/DeepCopyExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ICloneableMemberwiseAppTests
+{
+    [TestClass]
+    public partial class DeepCloneTests
+    {
+        [TestMethod]
+        public void DeepClone_Null_ReturnsDefault()
+        {
+            object obj = null;
+            var cloned = obj.DeepClone();
+            Assert.AreEqual(default(object), cloned);
+        }
+
+        [TestMethod]
+        public void DeepClone_ValueType_ReturnsClonedValue()
+        {
+            int value = 42;
+            var cloned = value.DeepClone();
+            Assert.AreEqual(value, cloned);
+        }
+
+        [TestMethod]
+        public void DeepClone_String_ReturnsClonedString()
+        {
+            string value = "hello";
+            var cloned = value.DeepClone();
+            Assert.AreEqual(value, cloned);
+        }
+
+        [TestMethod]
+        public void DeepClone_Array_ReturnsClonedArray()
+        {
+            int[] array = { 1, 2, 3 };
+            var cloned = array.DeepClone();
+            Assert.AreNotSame(array, cloned);
+            CollectionAssert.AreEqual(array, cloned);
+        }
+
+        [TestMethod]
+        public void DeepClone_Object_ReturnsClonedObject()
+        {
+            var obj = new TestObject
+            {
+                Name = "test",
+                Value = 42,
+                Child = new TestObject
+                {
+                    Name = "child",
+                    Value = 24
+                }
+            };
+
+            var cloned = obj.DeepClone();
+
+            Assert.AreNotSame(obj, cloned);
+            Assert.AreEqual(obj.Name, cloned.Name);
+            Assert.AreEqual(obj.Value, cloned.Value);
+            Assert.AreNotSame(obj.Child, cloned.Child);
+            Assert.AreEqual(obj.Child.Name, cloned.Child.Name);
+            Assert.AreEqual(obj.Child.Value, cloned.Child.Value);
+        }
+
+        private class TestObject
+        {
+            public string Name { get; set; }
+            public int Value { get; set; }
+            public TestObject Child { get; set; }
+        }
+    }
+}
