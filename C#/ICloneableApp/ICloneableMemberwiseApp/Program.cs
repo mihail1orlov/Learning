dcee69a7919c77c7b65d9f9b9e9c3481e6acfce4
Mihail Orlov <mihail_orlov@hotmail.com>
2023-03-14 12:05:20 +0200

    Gpt. Added private state to the ICloneableMemberwiseApp prj

--- C#/ICloneableApp/ICloneableMemberwiseApp/Program.cs
+++ C#/ICloneableApp/ICloneableMemberwiseApp/Program.cs
@@ -1,7 +1,8 @@
 ﻿// Создание объекта
-var original = new MyClass();
+
+var original = new MyClass("A33C30C4-7FEA-4827-8EBE-1D9426B87E2F");
 original.MyProperty = "Original";
-original.MyNestedClass = new MyNestedClass();
+original.MyNestedClass = new MyNestedClass("23EA7BF9-93FE-43A5-B0D4-A38C6326CD74");
 original.MyNestedClass.MyNestedProperty = "NestedOriginal";
 
 // Создание копии объекта
@@ -11,27 +12,37 @@ clone.MyNestedClass.MyNestedProperty = "NestedClone";
 
 // Вывод значений свойств
 Console.WriteLine("Original:");
-Console.WriteLine(original.MyProperty);
-Console.WriteLine(original.MyNestedClass.MyNestedProperty);
+Console.WriteLine(original);
 
 Console.WriteLine("Clone:");
-Console.WriteLine(clone.MyProperty);
-Console.WriteLine(clone.MyNestedClass.MyNestedProperty);
+Console.WriteLine(clone);
 
 Console.ReadLine();
-
 
 // Создаем класс, который будет использовать интерфейс ICloneable
 class MyClass : ICloneable
 {
+    private string _id;
+
+    public MyClass(string id)
+    {
+        _id = id;
+    }
+
     public string MyProperty { get; set; }
     public MyNestedClass MyNestedClass { get; set; }
+
+
+    public override string ToString()
+    {
+        return $"id: {_id}\tMyProperty: {MyProperty}\t{MyNestedClass}";
+    }
 
     // Реализация метода Clone
     public object Clone()
     {
         var clone = (MyClass)this.MemberwiseClone();
-        clone.MyNestedClass = (MyNestedClass)this.MyNestedClass.Clone();
+        clone.MyNestedClass = (MyNestedClass)MyNestedClass.Clone();
         return clone;
     }
 }
@@ -39,11 +50,22 @@ class MyClass : ICloneable
 // Создаем вложенный класс, который также будет использовать интерфейс ICloneable
 class MyNestedClass : ICloneable
 {
+    private string _id;
+
+    public MyNestedClass(string id)
+    {
+        _id = id;
+    }
+
     public string MyNestedProperty { get; set; }
 
+    public override string ToString()
+    {
+        return $"Id: {_id}\tMyNestedProperty: {MyNestedProperty}";
+    }
     // Реализация метода Clone
     public object Clone()
     {
-        return this.MemberwiseClone();
+        return (MyNestedClass)MemberwiseClone();
     }
 }
