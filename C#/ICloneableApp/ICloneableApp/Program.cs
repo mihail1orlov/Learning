a0bd7ba09a42275366e83f6f6c8c427db0369cc1
Mihail Orlov <mihail_orlov@hotmail.com>
2023-03-14 11:40:42 +0200

    Gpt. Added the ICloneable prj

---
+++ C#/ICloneableApp/ICloneableApp/Program.cs
@@ -0,0 +1,54 @@
+﻿// Создание объекта Person
+Person person1 = new Person("Peterson", "Peter", 25);
+
+// Создание копии объекта Person
+Person person2 = (Person)person1.Clone();
+
+// Вывод информации об объектах Person
+Console.WriteLine("Person 1: " + person1.ToString());
+Console.WriteLine("Person 2: " + person2.ToString());
+
+// Изменение свойств объекта Person 2
+person2.FirstName = "John";
+person2.Age = 30;
+
+// Вывод информации об объектах Person после изменения
+Console.WriteLine("\nPerson 1: " + person1.ToString());
+Console.WriteLine("Person 2: " + person2.ToString());
+
+Console.ReadLine();
+
+
+// Класс Person с реализацией ICloneable
+class Person : ICloneable
+{
+    // Поля класса
+    private string lastName;
+    private string firstName;
+    private int age;
+
+    // Конструктор класса
+    public Person(string lastName, string firstName, int age)
+    {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    // Свойства класса
+    public string LastName { get { return lastName; } set { lastName = value; } }
+    public string FirstName { get { return firstName; } set { firstName = value; } }
+    public int Age { get { return age; } set { age = value; } }
+
+    // Реализация метода Clone из интерфейса ICloneable
+    public object Clone()
+    {
+        return new Person(this.LastName, this.FirstName, this.Age);
+    }
+
+    // Переопределение метода ToString для вывода информации об объекте
+    public override string ToString()
+    {
+        return LastName + " " + FirstName + ", " + Age + " years old";
+    }
+}
---
+++ C#/ICloneableApp/ICloneableMemberwiseApp/Program.cs
@@ -0,0 +1,49 @@
+﻿// Создание объекта
+var original = new MyClass();
+original.MyProperty = "Original";
+original.MyNestedClass = new MyNestedClass();
+original.MyNestedClass.MyNestedProperty = "NestedOriginal";
+
+// Создание копии объекта
+var clone = (MyClass)original.Clone();
+clone.MyProperty = "Clone";
+clone.MyNestedClass.MyNestedProperty = "NestedClone";
+
+// Вывод значений свойств
+Console.WriteLine("Original:");
+Console.WriteLine(original.MyProperty);
+Console.WriteLine(original.MyNestedClass.MyNestedProperty);
+
+Console.WriteLine("Clone:");
+Console.WriteLine(clone.MyProperty);
+Console.WriteLine(clone.MyNestedClass.MyNestedProperty);
+
+Console.ReadLine();
+
+
+// Создаем класс, который будет использовать интерфейс ICloneable
+class MyClass : ICloneable
+{
+    public string MyProperty { get; set; }
+    public MyNestedClass MyNestedClass { get; set; }
+
+    // Реализация метода Clone
+    public object Clone()
+    {
+        var clone = (MyClass)this.MemberwiseClone();
+        clone.MyNestedClass = (MyNestedClass)this.MyNestedClass.Clone();
+        return clone;
+    }
+}
+
+// Создаем вложенный класс, который также будет использовать интерфейс ICloneable
+class MyNestedClass : ICloneable
+{
+    public string MyNestedProperty { get; set; }
+
+    // Реализация метода Clone
+    public object Clone()
+    {
+        return this.MemberwiseClone();
+    }
+}
