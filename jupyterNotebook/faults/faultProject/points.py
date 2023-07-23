4bbf906c6c69661ab44ed62d0ec4302a7e0d886f
Mihail Orlov <mihail_orlov@hotmail.com>
2023-07-23 17:53:30 +0000

    Added the point color

--- jupyterNotebook/faults/faultProject/points.py
+++ jupyterNotebook/faults/faultProject/points.py
@@ -1,6 +1,7 @@
 class Point2D:
-    def __init__(self, md, tvd, name=None, description=None):
+    def __init__(self, md, tvd, name=None, color=None, description=None):
         self.md = md
         self.tvd = tvd
         self.name = name
+        self.color = color
         self.description = description
