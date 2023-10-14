0cdd987ba8f1500f132e5d3e7ca8cc7c70da05c7
Mihail Orlov <mihail@orlov.com>
2023-10-14 13:27:16 +0200

    Added the wireguard instruction

---
+++ C#/MineClickApp/MineClickApp/BeautifulDelay.cs
@@ -0,0 +1,14 @@
+﻿public class BeautifulDelay
+{
+    static readonly char[] Spinner = { '|', '/', '-', '\\' };
+
+    public static void Animate(int durationInMilliseconds = 5000)
+    {
+        int iterations = durationInMilliseconds / 250;
+        for (int i = 0; i < iterations; i++)
+        {
+            Console.Write($"\rAnimation: {Spinner[i % 4]}");
+            Thread.Sleep(250);
+        }
+    }
+}
--- C#/MineClickApp/MineClickApp/Program.cs
+++ C#/MineClickApp/MineClickApp/Program.cs
@@ -1,18 +1,40 @@
 ﻿using WindowsInput;
 
+BeautifulDelay.Animate();
+
 var inputSimulator = new InputSimulator();
-for (var i = 0; true ; i++)
+
+for (var i = 0; true; i++)
 {
+    MoveForward(inputSimulator);
     Press(i, inputSimulator);
 }
 
-void Press(int i, InputSimulator inputSimulator)
+void TurnLeft(InputSimulator inputSimulator1)
 {
-    inputSimulator.Mouse.RightButtonDown();
+    // Имитация поворота (нажатие клавиши "A" для движения влево)wa
+    inputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.VK_A);
+    Console.WriteLine("Turning left");
+    Thread.Sleep(1000);  // Поворачиваем влево в течение 1 секундыwawa
+}
+
+static void MoveForward(InputSimulator inputSimulator)
+{
+    // Имитация нажатия клавиши "W" для движения вперед
+    inputSimulator.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_W);
+    Console.WriteLine("Moving forward");
+    Thread.Sleep(500);  // Двигаемся вперед в течение 1 секунды
+    inputSimulator.Keyboard.KeyUp(WindowsInput.Native.VirtualKeyCode.VK_W);
+}
+
+static void Press(int i, InputSimulator inputSimulator)
+{
+    // Имитация нажатия правой кнопки мыши
+    inputSimulator.Mouse.LeftButtonDown();
     Console.WriteLine($"{i}. RightButtonDown");
-    Thread.Sleep(4000);
-    
-    inputSimulator.Mouse.RightButtonUp();
-    Console.WriteLine($"{i}. RightButtonUp");    
-    Thread.Sleep(6000);
+    Thread.Sleep(4000);  // Держим кнопку в течение 4 секунд для добычи руды
+
+    // Отпускание правой кнопки мыши
+    inputSimulator.Mouse.LeftButtonUp();
+    Console.WriteLine($"{i}. {nameof(inputSimulator.Mouse.LeftButtonUp)}");
 }
