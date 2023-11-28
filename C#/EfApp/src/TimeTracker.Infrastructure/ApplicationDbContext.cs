869fe5ce53e6c01028aedc31dad6ca23dd807ea2
Mihail Orlov <mihail@orlov.com>
2023-11-28 22:44:52 +0100

    Fix migration packege. The Pomelo.EntityFrameworkCore.MySql the best

--- C#/EfApp/src/TimeTracker.Infrastructure/ApplicationDbContext.cs
+++ C#/EfApp/src/TimeTracker.Infrastructure/ApplicationDbContext.cs
@@ -8,5 +8,5 @@ public class ApplicationDbContext : DbContext
     public DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseMySQL("server=192.168.1.9;port=3306;database=timeTrackerDb;user=root;password=my-secret-pw");
+        => options.UseMySql("server=192.168.1.9;port=3306;database=timeTrackerDb;user=root;password=my-secret-pw", new MariaDbServerVersion("11.2.2"));
 }
