82d72ca82f653b613999ce930830ced40a7dd424
Mihail Orlov <mihail@orlov.com>
2023-12-06 08:31:21 +0100

    Added the Download YouTube Videos and Music page

---
+++ C#/YoutubeClientApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+var youtube = new YoutubeClient();
+
+//https://youtu.be/jxiwsaU3SJE?si=lqHdaoAnimhMGB_P
+var videoId = "jxiwsaU3SJE";
+var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoId);
+var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+
+var httpClient = new HttpClient();
+var stream = await httpClient.GetStreamAsync(streamInfo.Url);
+var filePath = Path.Combine("D:/tmp/", "video22.mp4");
+
+await using var fileStream = File.OpenWrite(filePath);
+await stream.CopyToAsync(fileStream);
